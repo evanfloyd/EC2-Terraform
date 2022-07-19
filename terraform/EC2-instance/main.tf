458e2f621b581748550ed0872b52ba7d942366dd
Evan Floyd <evan.floyd@idevops.io>
2022-07-19 14:28:13 -0700

    modified main.tf by removing s3 bucket

--- terraform/EC2-instance/main.tf
+++ terraform/EC2-instance/main.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "zebedee-test-terraform-remote-state"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
-
 resource "aws_instance" "ec2_in" {
   ami           = var.ami_id
   instance_type = var.instance_size
