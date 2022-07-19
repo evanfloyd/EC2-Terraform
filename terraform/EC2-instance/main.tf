44fc4578bd050e2135fe9c50ddbb1cdefb938a70
Evan Floyd <evan.floyd@idevops.io>
2022-07-19 15:09:45 -0700

    updated user data path

--- terraform/EC2-instance/main.tf
+++ terraform/EC2-instance/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2_in" {
   instance_type = var.instance_size
   subnet_id = var.subnet
 
-  user_data = "${file("user-data/install_docker.sh")}"
+  user_data = "${file("EC2-Terraform/user-data/install_docker.sh")}"
 
 
   tags = {
