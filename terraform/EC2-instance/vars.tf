39caabf2303ca91983665c0a6313d67458d4789d
Evan Floyd <evan.floyd@idevops.io>
2022-07-19 19:15:45 -0700

    merged the two terraform files

--- terraform/EC2-instance/vars.tf
+++ terraform/EC2-instance/vars.tf
@@ -21,3 +21,19 @@ variable "cidr_blocks" {
 variable "vpc_id" {
   default = "vpc-0d3dd930d8dfb97e3"
 }
+
+variable "availability_zone" {
+  default = "us-east-1"
+}
+
+variable "size" {
+  default = "8"
+}
+
+variable "name" {
+  default = "evans_volume"
+}
+
+variable "device_name" {
+  default = "/dev/sdh"
+}
